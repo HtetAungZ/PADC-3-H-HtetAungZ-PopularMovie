3fa5a8db062bdc6d2dea4f0f8d868db79347526e
DESKTOP-0DQPE5H\ICE <saberlinger98@gmail.com>
2017-12-22 20:00:25 +0630

    changing recycler view for dynamic data

---
+++ app/src/main/java/com/padcmyanmar/popularmovie/viewholders/ItemMoviesGenresViewHolder.java
@@ -0,0 +1,8 @@
+package com.padcmyanmar.popularmovie.viewholders;
+
+/**
+ * Created by ICE on 22-12-2017.
+ */
+
+public class ItemMoviesGenresViewHolder {
+}
---
+++ app/src/main/java/com/padcmyanmar/popularmovie/viewholders/ItemMoviesReviewsViewHolder.java
@@ -0,0 +1,10 @@
+package com.padcmyanmar.popularmovie.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by ICE on 22-12-2017.
+ */
+
+public class ItemMoviesReviewsViewHolder extends RecyclerView.ViewHolder {
+}
